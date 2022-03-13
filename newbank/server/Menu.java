39d1dcdf822e3352529344b820bbf4439d30957c
Tzenis Bimpa <TBimpa@spartaglobal.com>
2022-03-13 23:08:01 +0000

    Add menu class

---
+++ newbank/server/Menu.java
@@ -0,0 +1,11 @@
+package newbank.server;
+
+public class Menu {
+
+    public void display(){
+        System.out.println("Press 1 to display your accounts.");
+        System.out.println("Press 2 to create a new account.");
+        System.out.println("Press 3 to edit  your details.");
+        System.out.println("Press 4 to Request a loan.");
+    }
+}
